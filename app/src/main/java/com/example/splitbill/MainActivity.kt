14c0ef97e4c45feeebd3f5ac4b98ed01378c558c
Gozaly <ar-angelita.gozaly@jp-c02dj0rwmd6r.intra.rakuten.co.jp>
2023-09-27 14:47:24 +0900

    Split Bill First Commit

---
+++ app/src/main/java/com/example/splitbill/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.splitbill
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btSplitBill = findViewById<Button>(R.id.bt_split_bill)
+        btSplitBill.setOnClickListener{
+            val intent = Intent(this, SplitBill::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/splitbill/SplitBill.kt
@@ -0,0 +1,12 @@
+package com.example.splitbill
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class SplitBill : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_split_bill)
+    }
+}
