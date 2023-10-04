79ad20a0fb2b3222af76c358a3b5c3605ea627f5
angelitagozaly <cangelitag@gmail.com>
2023-10-04 15:57:07 +0900

    Refactored the code

--- app/src/main/java/com/example/splitbill/SplitBillActivity.kt
+++ app/src/main/java/com/example/splitbill/SplitBillActivity.kt
@@ -9,36 +9,47 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.splitbill.databinding.ActivitySplitBillBinding
 
 class SplitBillActivity : AppCompatActivity(), AdapterListener {
 
     private lateinit var adapter: UserAdapter
     private lateinit var userListRecyclerView: RecyclerView
     private var amount: Long = 0
+    private lateinit var binding: ActivitySplitBillBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_split_bill)
-        setSupportActionBar(findViewById(R.id.tb_split_bill_toolbar))
+        binding = ActivitySplitBillBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.tbSplitBillToolbar)
         supportActionBar?.title ="Back"
 
-        val paymentAmountTextView : TextView = findViewById(R.id.tv_value_amount)
+        setPaymentAmountTextView()
+        setRecyclerView()
+        setAddButtonListener()
+    }
+
+    fun setPaymentAmountTextView() {
+        val paymentAmountTextView : TextView = binding.tvValueAmount
         amount = intent.getIntExtra("paymentAmount", 0).toLong()
         paymentAmountTextView.text = amount.toString()
+    }
 
-        userListRecyclerView = findViewById<RecyclerView>(R.id.rv_user_list_recycler_view)
-
+    fun setRecyclerView() {
+        userListRecyclerView = binding.rvUserListRecyclerView
         adapter = UserAdapter()
         adapter.addAdapterListener(this)
         adapter.addParticipant(User(1, "Current User", amount, true))
+        userListRecyclerView.adapter = adapter
+        userListRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
 
-        val addButton = findViewById<TextView>(R.id.tv_plus_button)
+    fun setAddButtonListener(){
+        val addButton = binding.tvPlusButton
         addButton.setOnClickListener {
             addParticipant()
         }
-        userListRecyclerView.adapter = adapter
-
-        userListRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     fun addParticipant() {
@@ -62,63 +73,6 @@ class SplitBillActivity : AppCompatActivity(), AdapterListener {
         var amount: Long,
         val isCurrentUser: Boolean
     )
-
-    class UserAdapter() : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
-
-        inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-        private var mListener:AdapterListener? = null
-        private val userList = mutableListOf<User>()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.user_list_item, parent, false)
-            return UserViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-            val currentItem = userList[position]
-            val tvUserName = holder.itemView.findViewById<TextView>(R.id.tv_user_name)
-            val tvUserAmount = holder.itemView.findViewById<TextView>(R.id.tv_user_amount)
-            tvUserName.text = currentItem.name
-            tvUserAmount.text = currentItem.amount.toString()
-
-            val tvRemoveButton = holder.itemView.findViewById<TextView>(R.id.tv_remove_button)
-            if (currentItem.isCurrentUser){
-                tvRemoveButton.visibility = View.GONE
-            } else {
-                tvRemoveButton.visibility = View.VISIBLE
-            }
-
-            tvRemoveButton.setOnClickListener {
-                mListener?.onRemoveParticipant(currentItem)
-            }
-
-        }
-
-        override fun getItemCount() = userList.size
-
-        fun addAdapterListener(listener: AdapterListener){
-            this.mListener = listener
-        }
-
-        fun addParticipant(user: User) {
-            userList.add(user)
-            notifyDataSetChanged()
-        }
-
-        fun removeParticipant(user: User) {
-            userList.removeIf {
-                it.id == user.id
-            }
-            notifyDataSetChanged()
-        }
-
-        fun updateAmount(splitAmount: Long){
-            for (user in userList){
-                user.amount = splitAmount
-            }
-        }
-
-    }
 
     override fun onRemoveParticipant(user: User) {
         removeParticipant(user)
